86002f01f8b7f41efbe10d3238b8ab21cca1c4d0
Veer1578 <sweetyatulshah@gmail.com>
2025-04-04 09:33:50 +0530

    stringOp

---
+++ stringOp.py
@@ -0,0 +1,6 @@
+text = str(input("Enter a word :"))
+#Reverse string
+#Using step value as -1 to iterate in reverse
+revText = text[::-1]
+text = revText
+print(f"The reverse of the inputed string is {text}")
