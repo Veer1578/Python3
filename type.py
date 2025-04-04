6b92725c48812263244bd0fe6e04118cbf454b0c
Veer1578 <sweetyatulshah@gmail.com>
2025-04-04 09:12:17 +0530

    type

---
+++ type.py
@@ -0,0 +1,11 @@
+a = 5
+print("The type of a is", type(a))
+ 
+b = 6.25
+print("The type of b is", type(b))
+
+c = "Python"
+print("The type of c is", type(c))
+
+d = True
+print("The type of d is", type(d))
